e6cdf11149c59158d887f0a1fd3333e492bed7a6
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2022-05-12 01:10:11 +0200

    Update TheGUI.py

--- TheGUI.py
+++ TheGUI.py
@@ -19,6 +19,12 @@ img = ImageTk.PhotoImage(Image.open("BackGround.PNG"))
 l_image = tkinter.Label(root, image=img)
 l_image.pack()
 
+# T2 = tkinter.Text(root, height=2, width=300)
+# Fact = """The text to be written"""
+# T2.insert(tkinter.END, Fact)
+# T2.pack()
+# T2.place(x=0, y=250)
+
 # l_robot_words = tkinter.Label(root, text="")
 # l_robot_words.configure(fg='black')
 # l_robot_words.config(font=("Courier", 14))
@@ -26,8 +32,6 @@ l_image.pack()
 
 
 T = tkinter.Text(root, height=2, width=300)
-Fact = """The text to be written"""
-T.insert(tkinter.END, Fact)
 T.pack()
 T.place(x=0, y=250)
 
