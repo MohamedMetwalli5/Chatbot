31f5139016e8e158923d8b4757b531f8309190b4
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2022-05-12 12:11:29 +0200

    Update TheGUI.py

--- TheGUI.py
+++ TheGUI.py
@@ -1,13 +1,15 @@
+from chat import get_response, bot_name
 import tkinter
 from PIL import ImageTk, Image
 
-input = ""
+
 
 def GetInput():
-    inp = input_text_box.get(1.0, "end-1c")
-    input = inp
-    print(input)
-    # lbl.config(text=inp)
+    output_text_box.delete("1.0", "end")
+    input = input_text_box.get(1.0, "end-1c")
+    output = get_response(input)
+    input_text_box.delete("1.0","end")
+    output_text_box.insert(tkinter.INSERT, output)
 
 
 root = tkinter.Tk()
@@ -26,7 +28,6 @@ l_title.pack()
 output_text_box = tkinter.Text(root, height=2, width=300)
 output_text_box.configure(bg='yellow')
 output_text_box.pack()
-# output_text_box.place(x=0, y=250)
 
 
 input_text_box = tkinter.Text(root, height=2, width=300)
@@ -38,17 +39,6 @@ img = ImageTk.PhotoImage(Image.open("BackGround.PNG"))
 l_image = tkinter.Label(root, image=img)
 l_image.pack()
 
-# T2 = tkinter.Text(root, height=2, width=300)
-# Fact = """The text to be written"""
-# T2.insert(tkinter.END, Fact)
-# T2.pack()
-# T2.place(x=0, y=250)
-
-# l_robot_words = tkinter.Label(root, text="")
-# l_robot_words.configure(fg='black')
-# l_robot_words.config(font=("Courier", 14))
-# l_robot_words.pack()
-
 
 input_text_box = tkinter.Text(root, height=2, width=300)
 input_text_box.pack()
@@ -56,9 +46,10 @@ input_text_box.place(x=0, y=250)
 
 
 button = tkinter.Button(root, text="Send", command=GetInput)
-button.configure(bg='yellow')
+button.configure(bg='purple')
 button.configure(fg='black')
 button.pack()
 button.place(x=150, y=300)
+
 
 root.mainloop()
